a4b6b9bd3db63c17604e5f9d42c1b4908158e152
Michael <freefaller@gmail.com>
2017-07-10 12:35:32 -0600

    Retro Barcode Designer project refactored

--- MEAN/Angular/retro-barcode/src/app/app.component.ts
+++ MEAN/Angular/retro-barcode/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Retro Barcode Designer';
+  boxes = [0,1,2,3,4,5,6,7,8,9];
   colors = [
     '#008080',
     '#8A2BE2',
@@ -18,7 +19,7 @@ export class AppComponent {
     '#FFD700',
     '#FF8C00',
     '#7CFC00',
-  ]
+  ];
 
   getColor(){
     return this.colors[Math.floor(Math.random() * 10)+1];
